55b13a9f8d471ac7db1bac25ea02b9115cd9a2c0
Kevin Alexis Rodríguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-04-24 16:07:10 -0400

    feat q-4 UI

--- app-node/src/app.js
+++ app-node/src/app.js
@@ -1,17 +1,25 @@
 import express from "express";
 import { dirname } from "path";
+import os from "os";
+
+
+
 import bookRouter from "./routes/book.route.js";
 const app = express();
+const platform = os.platform();
 
-const __dirname = dirname(new URL(import.meta.url).pathname);
+const __dirname = dirname(new URL(import.meta.url).pathname).substring(platform === "win32" ? 1 : 0);
+
 
 
 app.use(express.json());
-app.use(express.static(__dirname + "/static/build"));
+app.use(express.static( __dirname+ "/static/build"));
 
 app.use("/api", bookRouter);
 
-console.log("dirname: ", __dirname);
+
+console.log(os.platform());
+console.log(__dirname);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/static/build/index.html");
